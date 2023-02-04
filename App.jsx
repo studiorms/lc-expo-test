d7c988a18eeef1720671846e074cc998a9dced2e
Robert Smith <roberts@studiorms.com>
2023-02-04 11:38:02 -0800

    Add Placeholder Stack Screens

--- App.jsx
+++ App.jsx
@@ -1,39 +1,24 @@
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { NavigationContainer } from "@react-navigation/native";
+import "react-native-gesture-handler";
 
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import { Button } from 'react-native-web';
+import { createStackNavigator } from "@react-navigation/stack";
+import React from "react";
+import { StyleSheet } from "react-native";
+import Home from "./screens/Home";
+import NewTweet from "./screens/NewTweet";
+import Profile from "./screens/Profile";
+import Tweet from "./screens/Tweet";
 
-function HomeScreen({navigation}) {
-    return (
-      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Text>Home Screen</Text>
-        <Button
-        title="Go to Details"
-        onPress={() => navigation.navigate('Details')}
-      />
-      </View>
-    );
-  }
-
-  function DetailsScreen() {
-    return (
-      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Text>Details Screen</Text>
-      </View>
-    );
-  }
-
-  const Stack = createNativeStackNavigator();
+const Stack = createStackNavigator();
 
 export default function App() {
-
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen name="Home" component={HomeScreen} />
-        <Stack.Screen name="Details" component={DetailsScreen} />
+        <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen name="New Tweet" component={NewTweet} />
+        <Stack.Screen name="Tweet" component={Tweet} />
+        <Stack.Screen name="Profile" component={Profile} />
       </Stack.Navigator>
     </NavigationContainer>
   );
@@ -42,19 +27,19 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     //flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    marginTop: 60
+    backgroundColor: "#fff",
+    alignItems: "center",
+    marginTop: 60,
     //justifyContent: 'center',
   },
   button: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    backgroundColor: '#DDDDDD',
+    flexDirection: "row",
+    alignItems: "center",
+    backgroundColor: "#DDDDDD",
     padding: 10,
   },
   countContainer: {
-    alignItems: 'center',
+    alignItems: "center",
     padding: 10,
   },
 });
