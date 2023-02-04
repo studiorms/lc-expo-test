e1b6286492b5bb03fa8a3b6b4d22859b22115228
Robert Smith <roberts@studiorms.com>
2023-02-04 15:45:47 -0800

    Add screen mockups and nav

--- App.jsx
+++ App.jsx
@@ -1,25 +1,111 @@
+import { FontAwesome, Ionicons } from "@expo/vector-icons";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { createDrawerNavigator } from "@react-navigation/drawer";
 import { NavigationContainer } from "@react-navigation/native";
-import "react-native-gesture-handler";
-
 import { createStackNavigator } from "@react-navigation/stack";
 import React from "react";
 import { StyleSheet } from "react-native";
+import "react-native-gesture-handler";
 import Home from "./screens/Home";
 import NewTweet from "./screens/NewTweet";
+import Notifications from "./screens/Notifications";
 import Profile from "./screens/Profile";
+import Search from "./screens/Search";
+import Settings from "./screens/Settings";
 import Tweet from "./screens/Tweet";
 
 const Stack = createStackNavigator();
+const HomeStackNavigator = () => {
+  return (
+    <Stack.Navigator
+      screenOptions={{
+        headerShown: true,
+        headerBackTitleVisible: false,
+      }}
+    >
+      <Stack.Screen
+        name="Tab"
+        component={TabNavigator}
+        options={{ headerShown: false }}
+      />
+      <Stack.Screen
+        name="New Tweet"
+        component={NewTweet}
+        options={{
+          title: "",
+        }}
+      />
+      <Stack.Screen
+        name="Tweet"
+        component={Tweet}
+        options={{
+          title: "",
+        }}
+      />
+      <Stack.Screen
+        name="Profile"
+        component={Profile}
+        options={{
+          title: "",
+        }}
+      />
+    </Stack.Navigator>
+  );
+};
+const Tab = createBottomTabNavigator();
+const TabNavigator = () => {
+  return (
+    <Tab.Navigator
+      screenOptions={{
+        headerShown: false,
+        tabBarShowLabel: false,
+      }}
+    >
+      <Tab.Screen
+        name="Home1"
+        component={Home}
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="home" size={size} color={color} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Search"
+        component={Search}
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="search" size={size} color={color} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Notifications"
+        component={Notifications}
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <FontAwesome name="bell-slash-o" size={size} color="color" />
+          ),
+        }}
+      />
+    </Tab.Navigator>
+  );
+};
+
+const Drawer = createDrawerNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Home" component={Home} />
-        <Stack.Screen name="New Tweet" component={NewTweet} />
-        <Stack.Screen name="Tweet" component={Tweet} />
-        <Stack.Screen name="Profile" component={Profile} />
-      </Stack.Navigator>
+      <Drawer.Navigator
+        initialRouteName="Home"
+        screenOptions={{
+          headerShown: true,
+        }}
+      >
+        <Drawer.Screen name="Home" component={HomeStackNavigator} />
+        <Drawer.Screen name="Settings" component={Settings} />
+      </Drawer.Navigator>
     </NavigationContainer>
   );
 }
